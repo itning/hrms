10bc74d02b47d46c18f077595233cce9b34a8a0b
ITNING <3579677768@qq.com>
2018-01-05 09:58:00 +0800

    CAS登录后显示用户名

--- src/main/java/top/itning/hrms/security/CustomUserDetailsServiceImpl.java
+++ src/main/java/top/itning/hrms/security/CustomUserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package top.itning.hrms.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,10 +14,11 @@ import java.util.Set;
  * @author Ning
  */
 public class CustomUserDetailsServiceImpl implements AuthenticationUserDetailsService<CasAssertionAuthenticationToken> {
+    private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailsServiceImpl.class);
 
     @Override
     public UserDetails loadUserDetails(CasAssertionAuthenticationToken token) throws UsernameNotFoundException {
-        System.out.println("当前的用户名是：" + token.getName());
+        logger.info("当前的用户名是：" + token.getName());
         /*这里我为了方便，就直接返回一个用户信息，实际当中这里修改为查询数据库或者调用服务什么的来获取用户信息*/
         UserInfo userInfo = new UserInfo();
         userInfo.setUsername("admin");
